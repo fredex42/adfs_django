03d984c08a3272e953b72a90e46629c4b503e4fb
Andy Gallagher <andy.gallagher@theguardian.com>
2020-05-07 18:14:46 +0100

    improving build by doing requirements ahead of app copy so it is not re-run each time. enable static file serving by uwsgi with offload. halt build if no frontend js available

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,15 @@
-#!/bin/bash
+#!/bin/bash -e
 
 if [ -f adfs_django.tar ]; then rm -f adfs_django.tar; fi
+if [ -f requirements.txt ]; then rm -f requirements.txt; fi
+
+if [ ! -f ../static/js/bundle.js ]; then
+  echo "bundle.js not found. Please run \'npm run build\' from the frontend directory before running"
+  echo "this script."
+  exit 1
+fi
 
 tar cv --exclude __pycache__ ../adfs_django ../static ../requirements.txt ../manage.py > adfs_django.tar
+cp ../requirements.txt .
 
 docker build . -t andyg42/adfs_django
